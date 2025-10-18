793e925c8e132d84c0dde2d5d4f9e1c820313b7b
emsabiq <136716581+emsabiq@users.noreply.github.com>
2025-10-18 13:37:23 +0700

    Update pixel.js

--- assets/pixel.js
+++ assets/pixel.js
@@ -1,20 +1,13 @@
-/* /assets/pixel.js */
-(function(){
-  const PIXEL_ID = (window.CONFIG && window.CONFIG.PIXEL_ID) || 'YOUR_PIXEL_ID';
+(function initPixel(){
+  const PID = (window.CONFIG && CONFIG.PIXEL_ID) || "";
+  if (!PID) return;
 
-  (function(f,b,e,v,n,t,s){
-    if(f.fbq) return; n=f.fbq=function(){ n.callMethod ? n.callMethod.apply(n,arguments):n.queue.push(arguments) };
-    if(!f._fbq) f._fbq=n; n.push=n; n.loaded=!0; n.version='2.0'; n.queue=[];
-    t=b.createElement(e); t.async=!0; t.src=v;
-    s=b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s);
-  })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+  !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+  n.push=n; n.loaded=!0; n.version='2.0'; n.queue=[]; t=b.createElement(e);
+  t.async=!0; t.src=v; s=b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s)
+  }(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
 
-  try {
-    window.fbq('init', PIXEL_ID);
-    window.fbq('track', 'PageView');
-  } catch (_) {}
-
-  window.safeTrack = function(event, params){
-    try { window.fbq('track', event, params || {}); } catch(_) {}
-  };
+  fbq('init', PID);
+  fbq('track', 'PageView');
 })();
