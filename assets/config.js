fb541bbc67fff4e142d13e8c8d875d2a849fd91b
emsabiq <136716581+emsabiq@users.noreply.github.com>
2025-10-18 12:25:44 +0700

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,7 +1,7 @@
 window.CONFIG = {
   PIXEL_ID: 'YOUR_PIXEL_ID',
   WORKER_BASE: 'https://gdc.msabiq-stan.workers.dev',
-  PUBLIC_ORIGIN: 'https://<domain-vercel-kamu>.vercel.app', // ganti saat deploy
+  PUBLIC_ORIGIN: 'https://gabungcuan.vercel.app', // ganti saat deploy
   PRODUCT_DEFAULT: {
     id: 'product-masterkit',
     name: 'Akses Digital Licence Masterkit U PLR MRR',
@@ -9,3 +9,4 @@ window.CONFIG = {
     currency: 'IDR'
   }
 };
+
